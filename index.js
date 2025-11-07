ca2b38e79a0ef70e55a23204ffa736ea62b58592
Redcliff <neelaazhakigithub@gmail.com>
2025-11-07 14:01:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+    "https://frontend-crud-reactjs.netlify.app"
+      ],
     methods: ["GET", "POST", "PATCH", "DELETE"],
   })
 );
